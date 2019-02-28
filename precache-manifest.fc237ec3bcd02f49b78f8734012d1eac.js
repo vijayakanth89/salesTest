3ca40fccda69747f30248e36b1bd8c956c388ded
Vijayakanth <vijayakanthblog@gmail.com>
2019-03-01 00:39:47 +0530

    first website code

---
+++ precache-manifest.fc237ec3bcd02f49b78f8734012d1eac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6a845eba9840b552b73a",
+    "url": "/static/js/main.6a845eba.chunk.js"
+  },
+  {
+    "revision": "40b857b26421f4e56d45",
+    "url": "/static/js/2.40b857b2.chunk.js"
+  },
+  {
+    "revision": "6a845eba9840b552b73a",
+    "url": "/static/css/main.d3353670.chunk.css"
+  },
+  {
+    "revision": "f65600390a29a0465d75ea3432026396",
+    "url": "/index.html"
+  }
+];
